3312dab064d8427914cab59fbb80f208f634aebf
kchen1023 <kristinchen1023@gmail.com>
2025-07-22 14:14:46 -0400

    Initial commit

---
+++ src/main/java/com/example/CatfruitTracker/CatfruitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CatfruitTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatfruitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatfruitTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CatfruitTracker/CatfruitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CatfruitTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatfruitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
