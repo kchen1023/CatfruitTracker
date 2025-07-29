1ba43ee8c79dc4d9aab6a3b1c86720a6313dcd15
Kristin Chen <131231802+kchen1023@users.noreply.github.com>
2025-07-29 00:04:50 -0400

    Add files via upload

--- DataScrapers/GetMaterialsAmount.py
+++ DataScrapers/GetMaterialsAmount.py
@@ -3,11 +3,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # load files
-df = pd.read_csv("battle_cats/super_rare_cats_merged.csv")
+input_file = pd.read_csv("battle_cats/super_rare_cats_merged.csv")
 
 results = []
 
-for _, row in df.iterrows():
+for _, row in input_file.iterrows():
     # pad left with 0 if only two digits to fit link
     formatted_cat_id = str(row['id']).zfill(3)
     cat = row['name']
@@ -19,23 +19,23 @@ for _, row in df.iterrows():
         if res.status_code != 200:
             print(f"❌ failed to retrieve page for {cat}")
             continue
-        
+
         soup = BeautifulSoup(res.content, "html.parser")
 
-        # Safely locate the True Form Costs table
         tf_div = soup.find("div", id="tfcollapse")
-        if not tf_div:
-            raise ValueError("Missing 'tfcollapse' div")
-
         table = tf_div.find("table")
-        if not table:
-            raise ValueError("Missing table inside 'tfcollapse'")
-
         rows = table.find_all("tr")
 
-        data_cells = rows[1].find_all("td")[1:]
-        numbers = [cell.text.strip().replace("×", "") for cell in data_cells]
+        # we need to grab the second row (index 1) and then grab the numbers starting from the
+        # second column (index 1) since the first number represent the xp needed, which is irrelevant
+        cells = rows[1].find_all("td")[1:]
+        # get rid of the "x" in the retrieved value (e.g., "x5" -> "5")
+        numbers = []
+        for cell in cells:
+            text = cell.text.strip().replace("x", "")
+            numbers.append(text)
 
+        # numbers = # of materials needed for each item for the evolution
         results.append({
             "id": str(row['id']),
             "name": cat,
@@ -43,14 +43,15 @@ for _, row in df.iterrows():
         })
 
     except Exception as e:
+        # UDP does have information on all of the cats
         results.append({
             "id": formatted_cat_id,
             "name": cat,
             "numbers": "N/A on UDP"
         })
 
-# Output results to CSV
-df_out = pd.DataFrame(results)
-print(df_out)
-df_out.to_csv("battle_cats/super_rare_cats_costs.csv", index=False)
-print("✅ Saved")
+df = pd.DataFrame(results)
+print(df)
+
+df.to_csv("battle_cats/super_rare_cats_costs.csv", index=False)
+print("✅ saved")
