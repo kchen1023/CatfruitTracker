28b7e5ddc5f387d245edc45a07bd1bf9e6bb7798
kchen1023 <kristinchen1023@gmail.com>
2025-08-02 20:45:05 -0400

    move to new directory

--- src/main/java/com/example/CatfruitTracker/service/CatService.java
+++ src/main/java/com/example/CatfruitTracker/service/CatService.java
@@ -1,13 +1,11 @@
-package com.example.CatfruitTracker;
+package com.example.CatfruitTracker.service;
 
 import com.example.CatfruitTracker.entity.Cat;
 import com.example.CatfruitTracker.repository.CatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 // @Component tells Spring that it should be managed by the Spring container
 // Spring will create an instance of this class and manage its life cycle
